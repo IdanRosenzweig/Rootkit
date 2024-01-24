a4618a60b4fbb5b16b67c6e9dfe7cace0147ba59
idan <idanro12@gmail.com>
2024-01-24 06:50:50 -0500

    build files changes

--- linux/build.sh
+++ linux/build.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# build file for linux rootkit
+
+# build target directory
 BUILD_DIR=/home/idan/CLionProjects/Rootkit/linux/build
 
-# linux rootkit
+# Cmake root directory of the rootkit
 ROOTKIT_SOURCE_DIR=/home/idan/CLionProjects/Rootkit/linux
+
+# Make root directory of the kernel module
 KM_SOURCE_DIR=/home/idan/CLionProjects/Rootkit/linux/kernel_access/kernel_module/
+
+# what to call the kernel module
 KM_NAME=my_km
 
 # create the build directory
 mkdir -p $BUILD_DIR
 
-# cmake the rootkit
+# Cmake the rootkit
 cd $BUILD_DIR
 cmake -DMODULE_DIR="\"$BUILD_DIR/\"" -DMODULE_NAME="\"$KM_NAME\"" $ROOTKIT_SOURCE_DIR
 make -j4
@@ -20,7 +27,7 @@ if [ $? -ne 0 ]; then
   exit
 fi
 
-# make the kernel module
+# Make the kernel module
 cd $KM_SOURCE_DIR
 make OUTPUT_DIR=$BUILD_DIR MOD_NAME=$KM_NAME
 
